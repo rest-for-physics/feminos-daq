6d823c2520a3c035ff0468bfafcfb437e13c6c4a
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2024-08-29 14:26:10 +0200

    attach

--- viewer/feminos-viewer.py
+++ viewer/feminos-viewer.py
@@ -26,6 +26,8 @@ plt.rcParams.update({'font.size': 18})
 lock = threading.Lock()
 
 
+# ssh://lobis@sultan.unizar.es:22/home/lobis/R02286_Calibration_109Cd_Vm_260_Vd_94_Pr_1.1_Gain_0x1_Shape_0xF_Clock_0x4.root
+
 class LimitedOrderedDict(OrderedDict):
     # An OrderedDict that has a maximum size and removes the oldest item when the size is exceeded
     def __init__(self, max_size):
@@ -1933,8 +1935,9 @@ class EventViewer:
             self.filepath = filename
 
         self.load_file()
+        self.plot_graph()
+        
         self.auto_update.select()
-        self.load_file()
 
     def load_file(self):
         if self.filepath is None:
